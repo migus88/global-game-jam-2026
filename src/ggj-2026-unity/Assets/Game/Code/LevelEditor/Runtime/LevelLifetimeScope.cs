bd68b953872390524345f03eea31ae2bedca0837
Yuri Sokolov <7556847+migus88@users.noreply.github.com>
2026-01-30 12:15:49 +0200

    first implementation

--- src/ggj-2026-unity/Assets/Game/Code/LevelEditor/Runtime/LevelLifetimeScope.cs
+++ src/ggj-2026-unity/Assets/Game/Code/LevelEditor/Runtime/LevelLifetimeScope.cs
@@ -1,9 +1,55 @@
+using Cysharp.Threading.Tasks;
+using Game.LevelEditor.Data;
+using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 
-public class LevelLifetimeScope : LifetimeScope
+namespace Game.LevelEditor.Runtime
 {
-    protected override void Configure(IContainerBuilder builder)
+    public class LevelLifetimeScope : LifetimeScope
     {
+        [SerializeField] private LevelData _levelToLoad;
+        [SerializeField] private LevelConfiguration _levelConfiguration;
+
+        protected override void Configure(IContainerBuilder builder)
+        {
+            if (_levelConfiguration != null)
+            {
+                builder.RegisterInstance(_levelConfiguration);
+            }
+
+            builder.Register<LevelBuilder>(Lifetime.Singleton);
+            builder.RegisterEntryPoint<LevelEntryPoint>();
+        }
+
+        private class LevelEntryPoint : IStartable
+        {
+            private readonly LevelBuilder _levelBuilder;
+            private readonly LevelLifetimeScope _scope;
+
+            [Inject]
+            public LevelEntryPoint(LevelBuilder levelBuilder, LevelLifetimeScope scope)
+            {
+                _levelBuilder = levelBuilder;
+                _scope = scope;
+            }
+
+            public void Start()
+            {
+                if (_scope._levelToLoad != null)
+                {
+                    BuildLevelAsync().Forget();
+                }
+                else
+                {
+                    Debug.LogWarning("LevelLifetimeScope: No LevelData assigned to load");
+                }
+            }
+
+            private async UniTaskVoid BuildLevelAsync()
+            {
+                await _levelBuilder.BuildLevelAsync(_scope._levelToLoad);
+            }
+        }
     }
 }
